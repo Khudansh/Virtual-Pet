b3bc2659723faf8027a6dd03e0d169f32ebffee1
Khudansh <70052624+Khudansh@users.noreply.github.com>
2020-12-02 21:49:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ var PLAY=1;
 var gameState=PLAY;
 
 function preload(){
-  dogImg=loadImage("images/dogImg.png");
-  happyDogImg=loadImage("images/dogImg1.png");
+  dogImg=loadImage("dogImg.png");
+  happyDogImg=loadImage("dogImg1.png");
 }
 
 function setup() {
